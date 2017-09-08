d08b02d82b6fcd6f6cd283a0fcbeca0f07225fe6
Roberto Iriondo <contact@robertoiriondo.com>
2017-09-08 10:28:42 -0400

    Create numbers.php
    Basic PHP - Operators and scalar variables on PHP

---
+++ numbers.php
@@ -0,0 +1,32 @@
+//Operators and scalar variables on PHP
+
+<?php
+  $num_1 = 0;
+  /*$num_2 = 2;
+  $num_3 = 3;*/
+  $d_home = 5.0;
+  $d_work = 5.6;
+
+  var_dump ($num_1 + $d_home + $d_work);
+
+  $a = 5;
+  /*$b = 10;
+
+  var_dump($a * $b);
+  var_dump($a / $b);
+
+  $a = $a + 1;
+  var_dump($a);
+  $a++;
+  var_dump($a); //++ = +1
+  $a--;
+  var_dump($a);//-- = -1
+  var_dump(--$a);//Does the substraction before the variable.
+  
+  $a = $a + 5;
+  var_dump($a);
+  $a += 5;//Produces the same outcome as the line above += 
+  var_dump($a);*/
+
+  var_dump($a++);//Dumps the current value of "a" before is incremented
+?>
